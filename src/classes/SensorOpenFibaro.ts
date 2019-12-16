72b5855959c6d907571c15eb409a7b12eb3bb5de
Antonov, Oleksiy <Oleksiy.Antonov@live.com>
2019-12-16 21:09:34 +0100

    0. Fix error

--- src/classes/SensorOpenFibaro.ts
+++ src/classes/SensorOpenFibaro.ts
@@ -2,7 +2,7 @@
 // model: Door/ Window Sensor FGK-10X
 // https://products.z-wavealliance.org/products/1356/configs
 
-import * as fibaroConsts from "../consts/fibaroConsts";
+import * as fibaroConsts from "../consts/FibaroConsts";
 import { SensorOpen } from "./SensorOpen";
 
 export class SensorOpenFibaro extends SensorOpen {
