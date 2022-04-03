f330a857b51a55da46237eff2996849d0a232806
Oleksiy Antonov <Oleksiy.Antonov@live.com>
2022-04-03 20:42:33 +0200

    refactor Initiator

--- src/classes/Initiator.ts
+++ src/classes/Initiator.ts
@@ -3,32 +3,32 @@ import * as globalConsts from "../consts/GlobalConsts";
 import { IInitiator } from "../interfaces/IInitiator";
 
 export class Initiator implements IInitiator {
-    private id: number;
-    private code: globalConsts.initiators;
+	private id: number;
+	private code: globalConsts.initiators;
 
-    constructor(paramCode: globalConsts.initiators) {
-        this.code = paramCode;
-        this.id = this.code;
-    }
+	constructor(paramCode: globalConsts.initiators) {
+		this.code = paramCode;
+		this.id = this.code;
+	}
 
-    public get Id(): number {
-        return this.id;
-    }
+	public get Id(): number {
+		return this.id;
+	}
 
-    public get Name(): string {
-        return globalConsts.initiatorsNames[this.code - 1];
-    }
+	public get Name(): string {
+		return globalConsts.initiatorsNames[this.code - 1];
+	}
 
-    public get Code(): globalConsts.initiators {
-        return this.code;
-    }
+	public get Code(): globalConsts.initiators {
+		return this.code;
+	}
 
-    SaveToStorage(): void {
-        //
-    }
+	SaveToStorage(): void {
+		//
+	}
 
-    RestoreFromStorage(): void {
-        //
-    }
+	RestoreFromStorage(): void {
+		//
+	}
 
 }
